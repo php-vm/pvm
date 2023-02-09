51f9d954a50a997f69f8112b31ca2a574b5b9d6f
Marius Karstedt <info@marius-karstedt.de>
2023-02-09 14:55:26 +0100

    Added files by Chris <chris@bogardo.com> from https://www.npmjs.com/package/php-version-manager

---
+++ index.js
@@ -0,0 +1,159 @@
+#!/usr/bin/env node
+
+const commander = require("commander");
+const chalk = require("chalk");
+const php = require("./php");
+const fpm = require("./fpm");
+const applicationVersion = require("../package.json").version;
+
+if (process.argv.length === 2) {
+  process.argv.push("status");
+}
+
+const renderStatus = () => {
+  console.log(
+    chalk`\n  {green PHP Version Manager} version {yellow ${applicationVersion}}\n`
+  );
+
+  const version = php.current();
+  const cli = php.moduleStatus(version, "cli", "xdebug");
+  const fpm = php.moduleStatus(version, "fpm", "xdebug");
+
+  const phpText = "PHP: " + chalk.blue.bold(version);
+  const cliText =
+    "CLI: " + (cli ? chalk.green.bold("ON") : chalk.red.bold("OFF"));
+  const fpmText =
+    "FPM: " + (fpm ? chalk.green.bold("ON") : chalk.red.bold("OFF"));
+
+  console.log("  " + [phpText, cliText, fpmText].join("   ") + "\n");
+};
+
+const program = new commander.Command();
+
+program
+  .name("pvm")
+  .version(
+    applicationVersion,
+    "-v, --version",
+    "output the current application version"
+  )
+  .usage("[command] [options]")
+  .description(
+    chalk`{green PHP Version Manager} version {yellow ${applicationVersion}}`
+  );
+
+program
+  .command("status")
+  .alias("s")
+  .description("Show current PHP version status")
+  .action(() => {
+    renderStatus();
+
+    process.exit(0);
+  });
+
+program
+  .command("ls")
+  .description("List available PHP versions")
+  .action(() => {
+    const currentVersion = php.current();
+    php.versions().forEach(version => {
+      if (version === currentVersion) {
+        console.log(chalk.green(version));
+      } else {
+        console.log(version);
+      }
+    });
+
+    process.exit(0);
+  });
+
+program
+  .command("use <version>")
+  .alias("u")
+  .description("Switch PHP version")
+  .action(version => {
+    if (/^\d\d$/.test(version)) {
+      version = version.slice(0, 1) + "." + version.slice(1);
+    }
+
+    if (php.use(version)) {
+      console.log("Restarting PHP-FPM and NGINX");
+      fpm.restart();
+    }
+
+    renderStatus();
+  });
+
+program
+  .command("xdebug [sapi] [status]")
+  .alias("x")
+  .description("Manage XDebug status")
+  .action((sapi, status) => {
+    const enableOptions = ["1", "on", "yes", "y"];
+    const disableOptions = ["0", "off", "no", "n"];
+
+    if (
+      status === undefined &&
+      (enableOptions.includes(sapi) || disableOptions.includes(sapi))
+    ) {
+      status = sapi;
+      sapi = undefined;
+    }
+
+    if (sapi !== undefined && !["fpm", "cli"].includes(sapi)) {
+      throw new Error(`Invalid SAPI \'${sapi}\', specify \'fpm\' or \'cli\'`);
+    }
+
+    if (status === undefined) {
+      php.moduleToggle("xdebug", sapi);
+    } else if (enableOptions.includes(status)) {
+      php.moduleEnable("xdebug", sapi);
+    } else if (disableOptions.includes(status)) {
+      php.moduleDisable("xdebug", sapi);
+    } else {
+      throw new Error(`Invalid status \'${status}\'`);
+    }
+
+    switch (status) {
+      case undefined:
+        break;
+      case "1":
+      case "on":
+      case "yes":
+      case "y":
+        php.moduleEnable("xdebug", sapi);
+        break;
+      case "0":
+      case "off":
+      case "no":
+      case "n":
+        php.moduleDisable("xdebug", sapi);
+        break;
+      default:
+        throw new Error(`Invalid status \'${status}\'`);
+    }
+
+    if (sapi === "fpm" || sapi === undefined) {
+      console.log("Restarting PHP-FPM and NGINX");
+      fpm.restart();
+    }
+
+    renderStatus();
+
+    process.exit(0);
+  });
+
+program
+  .command("restart")
+  .alias("r")
+  .description("Restart PHP-FPM and NGINX")
+  .action(() => {
+    console.log("Restarting PHP-FPM and NGINX");
+    fpm.restart();
+    process.exit(0);
+  });
+
+program.parse(process.argv);
+
+if (!program.args.length) renderStatus();
---
+++ php.js
@@ -0,0 +1,118 @@
+const { execSync } = require("child_process");
+
+/**
+ * Get currently active PHP version number
+ *
+ * @return {string}
+ */
+const current = () =>
+  execSync('php -v | head -n 1 | cut -d " " -f 2 | cut -f1-2 -d"."')
+    .toString()
+    .trim();
+
+/**
+ * Get all installed PHP version numbers
+ *
+ * @return {Array.string}
+ */
+const versions = () =>
+  execSync("find /usr/bin -name 'php*.*' -type f | cut -b 13- | sort -g")
+    .toString()
+    .trim()
+    .split("\n");
+
+/**
+ * Switch default PHP Version
+ *
+ * @param {string} version PHP Version number
+ *
+ * @return {bool|string}
+ */
+const use = version => {
+  if (!/^\d\.\d$/.test(version) || !versions().includes(version)) {
+    throw new Error(`Invalid version number "${version}"`);
+  }
+
+  if (version === current()) {
+    return false;
+  }
+
+  execSync(
+    `sudo /usr/bin/update-alternatives --set php /usr/bin/php${version}`
+  );
+
+  return current();
+};
+
+/**
+ * Get the status of a PHP module
+ *
+ * @param {string} version PHP Version
+ * @param {string} sapi    SAPI name (cli or fpm)
+ * @param {string} module  PHP Module name
+ *
+ * @return {boolean}
+ */
+const moduleStatus = (version, sapi, module) => {
+  try {
+    execSync(`phpquery -v ${version} -s ${sapi} -m ${module}`);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+/**
+ * Enable PHP Module
+ *
+ * @param {string} module
+ * @param {string|undefined} sapi
+ */
+const moduleEnable = (module, sapi) => {
+  sapi = sapi ? `-s ${sapi}` : "";
+  execSync(`sudo /usr/sbin/phpenmod ${sapi} ${module}`);
+};
+
+/**
+ * Disable PHP Module
+ *
+ * @param {string} module
+ * @param {string|undefined} sapi
+ */
+const moduleDisable = (module, sapi) => {
+  sapi = sapi ? `-s ${sapi}` : "";
+  execSync(`sudo /usr/sbin/phpdismod ${sapi} ${module}`);
+};
+
+const moduleToggle = (module, sapi) => {
+  const currentVersion = current();
+
+  let cliStatus;
+  let fpmStatus;
+
+  if (sapi === "cli" || sapi === undefined) {
+    cliStatus = moduleStatus(currentVersion, "cli", module);
+  }
+
+  if (sapi === "fpm" || sapi === undefined) {
+    fpmStatus = moduleStatus(currentVersion, "fpm", module);
+  }
+
+  if (cliStatus === true || fpmStatus === true) {
+    moduleDisable(module, sapi);
+    return false;
+  } else {
+    moduleEnable(module, sapi);
+    return true;
+  }
+};
+
+module.exports = {
+  current,
+  versions,
+  use,
+  moduleStatus,
+  moduleEnable,
+  moduleDisable,
+  moduleToggle
+};
